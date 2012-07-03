aac4053f993b84ec371439a87516d7d9c12047ac
Nambi Adhimoolam <nambi.adhimoolam@gmail.com>
2012-07-02 21:54:26 -0400

    solving project euler problems

--- java/test/org/nava/obj2map/ObjectToMapConverterTest.java
+++ java/test/org/nava/obj2map/ObjectToMapConverterTest.java
@@ -3,22 +3,30 @@ package org.nava.obj2map;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ObjectToMapConverterTest {
+/**
+ * 
+ * @author nambi
+ * 
+ */
+public class ObjectToMapConverterTest
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		ItemInContext item = new ItemInContext(10L, "TestItem", 12.34);
 		Map<String, Object> map = new HashMap<String, Object>();
-		
+
 		ObjectToMapConverter converter = new ObjectToMapConverter(item, map);
-		System.out.println("Item to be converted : "+item);
+		System.out.println("Item to be converted : " + item);
 		converter.convert();
-		
-		for (String key : map.keySet()) {
+
+		for (String key : map.keySet())
+		{
 			System.out.println("Key : " + key + " -> " + map.get(key));
 		}
-		
-		Long id = (Long)map.get("id");
-		System.out.println("The id maintains the type so I can increment by 2 :"+(id+2));
+
+		Long id = (Long) map.get("id");
+		System.out.println("The id maintains the type so I can increment by 2 :" + (id + 2));
 	}
-	
+
 }
