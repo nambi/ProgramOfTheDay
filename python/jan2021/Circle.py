3e68c3e1bed61a60e4b0fa3bd5ccad0ee6a7a66c
nambi <nambi.adhimoolam@gmail.com>
2021-01-14 20:07:50 -0500

    teaching my child python

---
+++ python/jan2021/Circle.py
@@ -0,0 +1,12 @@
+
+from turtle import *
+print "Hello Anbini"
+pendown()
+for n in range(24):
+	forward(10)
+	right(5)
+penup()
+
+print "Congrats"
+
+
---
+++ python/jan2021/interview.py
@@ -0,0 +1,17 @@
+
+print "Hello Dear, Welcome to AAS clinic \n Station X, Moon"
+
+print "Lets answer some basic questions for an appointment"
+
+name = raw_input("Whats your name : ")
+
+print "Thank you"
+
+place = raw_input("where do you live ? ")
+
+print "Great"
+
+if place == 'earth' :
+	print "Thank you "+name+". we dont offer service at "+place +" or even on any earth places yet."
+else :
+	print "Thank you "+name+". Nice to know that you live at "+place +". How May I help you?"
