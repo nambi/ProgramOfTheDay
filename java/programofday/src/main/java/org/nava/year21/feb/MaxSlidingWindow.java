35ba70b9b622fc3e5e9f4214cbbe227dff3ce761
nambi <nambi.adhimoolam@gmail.com>
2021-02-21 21:59:51 -0500

    fix lint

--- java/programofday/src/main/java/org/nava/year21/feb/MaxSlidingWindow.java
+++ java/programofday/src/main/java/org/nava/year21/feb/MaxSlidingWindow.java
@@ -1,24 +1,26 @@
 package org.nava.year21.feb;
 
+import java.util.ArrayDeque;
+
 /**
  * Given a large array of integers and a window of size
- w
- w, find the current maximum value in the window as the window slides through the entire array.
+ * w
+ * w, find the current maximum value in the window as the window slides through the entire array.
  */
-class MaxSlidingWindow{
+class MaxSlidingWindow {
     public static ArrayDeque<Integer> findMaxSlidingWindow(int[] arr, int windowSize) {
         ArrayDeque<Integer> result = new ArrayDeque<>();
 
-        int maxIndex=0;
+        int maxIndex = 0;
 
-        for(int i=0; i<=arr.length-windowSize; i++){
+        for (int i = 0; i <= arr.length - windowSize; i++) {
             int max = arr[i];
-            int j=i+1;
-            maxIndex = i+windowSize;
+            int j = i + 1;
+            maxIndex = i + windowSize;
             maxIndex = maxIndex;
-            while(j < maxIndex){
+            while (j < maxIndex) {
                 int currentValue = arr[j];
-                if(currentValue > max){
+                if (currentValue > max) {
                     max = currentValue;
                 }
                 j++;
@@ -29,13 +31,11 @@ class MaxSlidingWindow{
         return result;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        ArrayDeque<Integer> result = findMaxSlidingWindow([1, 2, 3, 4, 3, 2, 1, 2, 5],4);
-
-        for(int r : result){
-            System.out.println("Result : "+r);
-        }
+        int[] input = {1, 2, 3, 4, 3, 2, 1, 2, 5};
+        ArrayDeque<Integer> result = findMaxSlidingWindow(input,4);
+        System.out.println(result);
     }
 
 }
