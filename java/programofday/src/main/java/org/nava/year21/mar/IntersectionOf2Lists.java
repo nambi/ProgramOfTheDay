9c6ac63bead8548ece658959753855c9e6726562
nambi <nambi.adhimoolam@gmail.com>
2021-03-14 16:00:10 -0400

    intersection of points

--- java/programofday/src/main/java/org/nava/year21/mar/IntersectionOf2Lists.java
+++ java/programofday/src/main/java/org/nava/year21/mar/IntersectionOf2Lists.java
@@ -11,7 +11,46 @@ public class IntersectionOf2Lists {
 
     public static LinkedListNode intersectionOptimized(LinkedListNode head1, LinkedListNode head2) {
 
+        LinkedListNode list1node = null;
+        int list1length = get_length(head1);
+        LinkedListNode list2node = null;
+        int list2length = get_length(head2);
+
+        int length_difference = 0;
+        if (list1length >= list2length) {
+            length_difference = list1length - list2length;
+            list1node = head1;
+            list2node = head2;
+        } else {
+            length_difference = list2length - list1length;
+            list1node = head2;
+            list2node = head1;
+        }
+
+        while (length_difference > 0) {
+            list1node = list1node.next;
+            length_difference--;
+        }
+
+        while (list1node != null) {
+            if (list1node == list2node) {
+                return list1node;
+            }
+
+            list1node = list1node.next;
+            list2node = list2node.next;
+        }
         return null;
+    }
+
+    private static int get_length(
+        LinkedListNode head) {
+        int list_length = 0;
+        while (head != null) {
+            head = head.next;
+            list_length++;
+        }
+        return list_length;
     }
 
     public static LinkedListNode intersection(LinkedListNode head1, LinkedListNode head2) {
@@ -58,7 +97,7 @@ public class IntersectionOf2Lists {
         System.out.println("Print the list for 2 :" + Arrays.toString(input2));
         LinkedList.display(head2);
 
-        LinkedListNode match = intersection(head1, head2);
+        LinkedListNode match = intersectionOptimized(head1, head2);
         System.out.println("Match  is :");
         LinkedList.display(match);
 
