ea67fd39d41e4abda2c6ec3c5acc4208fb52b84d
nambi <nambi.adhimoolam@gmail.com>
2021-04-03 21:58:09 -0400

    Identical BTrees

---
+++ java/programofday/src/main/java/org/nava/year21/apr/BTreeIdenticalCheck.java
@@ -0,0 +1,33 @@
+package org.nava.year21.apr;
+
+/**
+ * Given the roots of two binary trees, determine if these trees are identical or not.
+ * Identical trees have the same layout and data at each node
+ */
+public class BTreeIdenticalCheck {
+
+
+    public static boolean isIdentical(BinaryTreeNode root1, BinaryTreeNode root2) {
+
+        return false;
+    }
+
+    public static boolean test1() {
+
+        return false;
+    }
+
+    public static boolean doTestsPass() {
+
+        return test1();
+    }
+
+    public static void main(String[] args) {
+
+        if (doTestsPass()) {
+            System.out.println("All Tests passed");
+        } else {
+            System.out.println("Tests failed");
+        }
+    }
+}
