9f9de29f93a1657ca7ed92c97e0e1d836b92e72c
nambi <nambi.adhimoolam@gmail.com>
2021-04-06 20:44:46 -0400

    BTree - in order traversal

--- java/programofday/src/main/java/org/nava/year21/apr/BTreeInorderTraversal.java
+++ java/programofday/src/main/java/org/nava/year21/apr/BTreeInorderTraversal.java
@@ -1,5 +1,7 @@
 package org.nava.year21.apr;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 /**
@@ -7,27 +9,37 @@ import java.util.Stack;
  */
 public class BTreeInorderTraversal {
 
-    private static Stack<BinaryTreeNode> stk = new Stack<BinaryTreeNode>();
 
-    public static void populateIterator(BinaryTreeNode root) {
-        while (root != null) {
-            stk.push(root);
-            root = root.left;
+    static String iterativeInorder(BinaryTreeNode root) {
+        String output = "";
+        Stack<BinaryTreeNode> stk = new Stack<BinaryTreeNode>();
+        while (!stk.empty() || root != null) {
+            if (root != null) {
+                stk.push(root);
+                root = root.left;
+                continue;
+            }
+            output += stk.peek().data + " ";
+            System.out.println(stk.peek().data + " ");
+            root = stk.pop().right;
         }
-    }
 
-    public static void getInOrder(BinaryTreeNode root) {
-        if (stk.isEmpty())
-            return;
-
-        BinaryTreeNode rVal = stk.pop();
-        BinaryTreeNode temp = rVal.right;
-        populateIterator(temp);
+        return output;
     }
 
     public static boolean test1() {
 
-        boolean result = (stk != null);
+        List<Integer> list = new ArrayList<>();
+        list.add(100);
+        list.add(200);
+        list.add(50);
+        list.add(75);
+        list.add(35);
+        list.add(25);
+        BinaryTreeNode root = BinaryTree.createBST(list);
+        String output = iterativeInorder(root);
+
+        boolean result = (output != null && output.trim().equalsIgnoreCase("25 35 50 75 100 200"));
         System.out.println("Test1 result " + result);
         return result;
     }
