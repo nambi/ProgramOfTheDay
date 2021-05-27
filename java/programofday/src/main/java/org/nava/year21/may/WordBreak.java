7201e03ff3357ab37d5d765a20df6c782ccaf350
nambi <nambi.adhimoolam@gmail.com>
2021-05-26 23:32:27 -0400

    word break

--- java/programofday/src/main/java/org/nava/year21/may/WordBreak.java
+++ java/programofday/src/main/java/org/nava/year21/may/WordBreak.java
@@ -1,12 +1,32 @@
 package org.nava.year21.may;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * Find out the word is made up of the dictionary words
  */
 public class WordBreak {
+
+    static boolean wordBreakNew(String s, List<String> wordDict) {
+        return wordBreakMemo(s, new HashSet<>(wordDict), 0, new Boolean[s.length()]);
+    }
+
+    static boolean wordBreakMemo(String s, Set<String> wordDict, int start, Boolean[] memo) {
+        if (start == s.length()) {
+            return true;
+        }
+        if (memo[start] != null) {
+            return memo[start];
+        }
+        for (int end = start + 1; end <= s.length(); end++) {
+            String sub = s.substring(start, end);
+            if (wordDict.contains(sub) && wordBreakMemo(s, wordDict, end, memo)) {
+                System.out.println("sub " + sub + " is in the dict");
+                return memo[start] = true;
+            }
+        }
+        return memo[start] = false;
+    }
 
     static boolean wordBreak(String s, List<String> wordDict) {
 
@@ -18,20 +38,38 @@ public class WordBreak {
         int startIndex = 0;
         while (endIndex <= s.length()) {
             String sub = s.substring(startIndex, endIndex);
-            System.out.println("sub "+sub);
+            System.out.println("sub " + sub);
             if (wordDict.contains(sub)) {
                 startIndex = endIndex;
-                System.out.println("sub "+sub+" is in the dict");
+                System.out.println("sub " + sub + " is in the dict");
             }
             endIndex++;
         }
         return (startIndex == s.length());
     }
 
+    static boolean test4() {
+        String s = "aaaaaaa";
+        String[] wordDict = {"aaaa", "aaa"};
+        ArrayList<String> list = new ArrayList<String>();
+        for (String w : wordDict) {
+            list.add(w);
+        }
+        boolean output = wordBreakNew(s, list);
+        boolean result = (output == true);
+        System.out.println("Test4 result " + result);
+        return result;
+    }
+
+
     static boolean test3() {
         String s = "applepenapple";
-        String[] wordDict = {"apple","pen"};
-        boolean output = wordBreak(s, Arrays.asList(wordDict));
+        String[] wordDict = {"apple", "pen"};
+        ArrayList<String> list = new ArrayList<String>();
+        for (String w : wordDict) {
+            list.add(w);
+        }
+        boolean output = wordBreak(s, list);
         boolean result = (output == true);
         System.out.println("Test3 result " + result);
         return result;
@@ -39,8 +77,12 @@ public class WordBreak {
 
     static boolean test2() {
         String s = "catsandog";
-        String[] wordDict = {"cats","dog","sand","and","cat"};
-        boolean output = wordBreak(s, Arrays.asList(wordDict));
+        String[] wordDict = {"cats", "dog", "sand", "and", "cat"};
+        ArrayList<String> list = new ArrayList<String>();
+        for (String w : wordDict) {
+            list.add(w);
+        }
+        boolean output = wordBreak(s, list);
         boolean result = (output == false);
         System.out.println("Test2 result " + result);
         return result;
@@ -50,7 +92,11 @@ public class WordBreak {
 
         String s = "leetcode";
         String[] wordDict = {"leet", "code"};
-        boolean output = wordBreak(s, Arrays.asList(wordDict));
+        ArrayList<String> list = new ArrayList<String>();
+        for (String w : wordDict) {
+            list.add(w);
+        }
+        boolean output = wordBreak(s, list);
         boolean result = (output == true);
         System.out.println("Test1 result " + result);
         return result;
@@ -58,7 +104,7 @@ public class WordBreak {
 
     static boolean doAllTestsPass() {
 
-        return test1() && test2() && test3();
+        return test1() && test2() && test3() && test4();
     }
 
     public static void main(String[] args) {
