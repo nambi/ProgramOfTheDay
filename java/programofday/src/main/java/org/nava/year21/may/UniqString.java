ef29907adbbfa6f285b0d51b7458c88e3921de61
nambi <nambi.adhimoolam@gmail.com>
2021-05-29 08:38:07 -0400

    unique characters

--- java/programofday/src/main/java/org/nava/year21/may/UniqString.java
+++ java/programofday/src/main/java/org/nava/year21/may/UniqString.java
@@ -4,11 +4,11 @@ public class UniqString {
 
     public static void main(String[] args) {
 
-        System.out.println(isUnique("abc"));
-        System.out.println(isUnique("abdac"));
+        System.out.println(isUnique2("abc"));
+        System.out.println(isUnique2("abdac"));
 
     }
-
+        //Brute force  O (n^2) or O (n log n)
     private static boolean isUnique(String input) {
         char[] inputArr = input.toCharArray();
 
@@ -20,6 +20,19 @@ public class UniqString {
 
             }
 
+        }
+        return true;
+    }
+    //Map O (n)
+    private static boolean isUnique2(String input) {
+        char[] inputArr = input.toCharArray();
+        //if ASCII, it will be 128, for extended ascii it will be 256
+        boolean[] charSet = new boolean[128];
+        for (int i = 0; i < inputArr.length; i++) {
+            if (charSet[inputArr[i]]) {
+                return false;
+            }
+            charSet[inputArr[i]] = true;
         }
         return true;
     }
