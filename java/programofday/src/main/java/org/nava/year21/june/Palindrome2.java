15b9a08fe11a67c94859686d8ae96eba4a9d4f9a
nambi <nambi.adhimoolam@gmail.com>
2021-06-20 20:06:45 -0400

    palindrome with iterating a character

---
+++ java/programofday/src/main/java/org/nava/year21/june/Palindrome2.java
@@ -0,0 +1,50 @@
+package org.nava.year21.june;
+
+/**
+ * Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+ */
+public class Palindrome2 {
+
+
+    static boolean isPalindromeAfterDelete(String s) {
+        //Ammas
+        /**
+         * iterate and delete a string and reverse string to match it
+         */
+        StringBuilder sb = new StringBuilder(s);
+        if (s.equalsIgnoreCase(sb.reverse().toString())) {
+            return true;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            sb = new StringBuilder(s);
+            sb.deleteCharAt(i);
+            if (sb.toString().equalsIgnoreCase(sb.reverse().toString())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static boolean test1() {
+
+        boolean output = isPalindromeAfterDelete("Ammas");
+        boolean result = (output == true);
+        System.out.println("test1 result " + result);
+        return result;
+    }
+
+    static boolean doAllTestsPass() {
+
+        return test1();
+    }
+
+    public static void main(String[] args) {
+
+        if (doAllTestsPass()) {
+            System.out.println("Tests pass");
+        } else {
+            System.out.println("Tests failed");
+        }
+
+    }
+}
